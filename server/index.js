89ec2f4a31190f888cb7c1f08746469d9382d7f8
jorovernier <55214971+jorovernier@users.noreply.github.com>
2020-05-31 12:59:55 -0700

    fixed certificate error

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env;
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const express = require('express');
 const app = express();
 app.use(express.json());
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     createProxyMiddleware({
         target: 'http://localhost:4000',
         changeOrigin: true,
-        secure: false
+        secure: true
       }
     )
   );
@@ -15,7 +15,7 @@ module.exports = function(app) {
     createProxyMiddleware({
         target: 'http://localhost:4000',
         changeOrigin: true,
-        secure: false
+        secure: true
       }
     )
   );
